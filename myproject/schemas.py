a2208e830372ee68e7f7865cda7349ef615d4790
yorbenwij <91123168+yorbenwij@users.noreply.github.com>
2023-12-28 14:36:17 +0100

    Update schemas.py

--- myproject/schemas.py
+++ myproject/schemas.py
@@ -16,9 +16,12 @@ class People(PeopleBase):
     class Config:
         orm_mode = True
 
+class CityBase(BaseModel):
+    name: str
+    population: int
+
 class CountryBase(BaseModel):
     name: str
-
 
 class CountryCreate(CountryBase):
     population: int
@@ -26,6 +29,18 @@ class CountryCreate(CountryBase):
 class Country(CountryBase):
     id: int
     civilians: list[People] = []
+
+    class Config:
+        orm_mode = True
+class UserBase(BaseModel):
+    name: str
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserRead(UserBase):
+    id: int
 
     class Config:
         orm_mode = True
