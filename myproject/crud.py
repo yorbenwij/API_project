05ceac38fb7c42e6c72ea2a376e41293a76478c5
yorbenwij <91123168+yorbenwij@users.noreply.github.com>
2023-12-28 14:34:58 +0100

    Update crud.py

--- myproject/crud.py
+++ myproject/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 
 import models
 import schemas
+from passlib.hash import bcrypt
 
 
 def get_country(db: Session, country_id: int):
@@ -33,3 +34,34 @@ def create_country_civilians(db: Session, civilians: schemas.PeopleCreate, count
     db.refresh(db_people)
     return db_people
 
+def update_country(db: Session, country_id: int, updated_country: schemas.CountryCreate):
+    db_country = db.query(models.Country).filter(models.Country.id == country_id).first()
+    if db_country:
+        for key, value in updated_country.dict().items():
+            setattr(db_country, key, value)
+        db.commit()
+        db.refresh(db_country)
+        return db_country
+
+def delete_country(db: Session, country_id: int):
+    db_country = db.query(models.Country).filter(models.Country.id == country_id).first()
+    if db_country:
+        db.delete(db_country)
+        db.commit()
+        return db_country
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name, email=user.email, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_name(db: Session, name: str):
+    return db.query(models.User).filter(models.User.name == name).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.User).offset(skip).limit(limit).all()
